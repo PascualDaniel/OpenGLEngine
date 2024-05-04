64240228c0c87a069387383c254476ce1400be3e
Daniel Pascual López <uo269728@uniovi.es>
2024-05-04 22:43:34 +0200

    Cambios en la clase textura, falta extraer codigo

--- OpenGLEngine/Textures/Texture.cpp
+++ OpenGLEngine/Textures/Texture.cpp
@@ -1,53 +1,69 @@
 #include "Texture.hpp"
 
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h" 
-
-
-
-
-void Texture::Generate(GLuint width, GLuint height, unsigned char* data)
+Texture::Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType)
 {
-    this->Width = width;
-    this->Height = height;
+	// Assigns the type of the texture ot the texture object
+	type = texType;
 
-    glBindTexture(GL_TEXTURE_2D, this->ID);
-    glTexImage2D(GL_TEXTURE_2D, 0, this->Internal_Format, width, height, 0, this->Image_Format, GL_UNSIGNED_BYTE, data);
+	// Stores the width, height, and the number of color channels of the image
+	int widthImg, heightImg, numColCh;
+	// Flips the image so it appears right side up
+	stbi_set_flip_vertically_on_load(true);
+	// Reads the image from a file and stores it in bytes
+	unsigned char* bytes = stbi_load(image, &widthImg, &heightImg, &numColCh, 0);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, this->Wrap_S);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, this->Wrap_T);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, this->Filter_Min);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, this->Filter_Max);
+	// Generates an OpenGL texture object
+	glGenTextures(1, &ID);
+	// Assigns the texture to a Texture Unit
+	glActiveTexture(slot);
+	glBindTexture(texType, ID);
 
-    glBindTexture(GL_TEXTURE_2D, 0);
+	// Configures the type of algorithm that is used to make the image smaller or bigger
+	glTexParameteri(texType, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+	glTexParameteri(texType, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	// Configures the way the texture repeats (if it does at all)
+	glTexParameteri(texType, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(texType, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// Extra lines in case you choose to use GL_CLAMP_TO_BORDER
+	// float flatColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	// glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, flatColor);
+
+	// Assigns the image to the OpenGL Texture object
+	glTexImage2D(texType, 0, GL_RGBA, widthImg, heightImg, 0, format, pixelType, bytes);
+	// Generates MipMaps
+	glGenerateMipmap(texType);
+
+	// Deletes the image data as it is already in the OpenGL Texture object
+	stbi_image_free(bytes);
+
+	// Unbinds the OpenGL Texture object so that it can't accidentally be modified
+	glBindTexture(texType, 0);
 }
 
-void Texture::Bind() const
+void Texture::texUnit(Shader& shader, const char* uniform, GLuint unit)
 {
-    glBindTexture(GL_TEXTURE_2D, this->ID);
+	// Gets the location of the uniform
+	GLuint texUni = glGetUniformLocation(shader.ID, uniform);
+	// Shader needs to be activated before changing the value of a uniform
+	shader.Activate();
+	// Sets the value of the uniform
+	glUniform1i(texUni, unit);
 }
 
-void Texture::loadFromFile(const std::string& path) {
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        glGenTextures(1, &textureID);
-        glBindTexture(GL_TEXTURE_2D, textureID); 
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);   
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
+void Texture::Bind()
+{
+	glBindTexture(type, ID);
 }
 
+void Texture::Unbind()
+{
+	glBindTexture(type, 0);
+}
+
+void Texture::Delete()
+{
+	glDeleteTextures(1, &ID);
+}
--- OpenGLEngine/Textures/Texture.hpp
+++ OpenGLEngine/Textures/Texture.hpp
@@ -3,27 +3,24 @@
 
 #include <glad/glad.h> // include glad to get all the required OpenGL headers
 
+#include "Shader.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 class Texture {
 public:
-    // Holds the ID of the texture object, used for all texture operations to reference to this particular texture
-    GLuint ID;
-    // Texture image dimensions
-    GLuint Width, Height; // Width and height of loaded image in pixels
-    // Texture Format
-    GLuint Internal_Format; // Format of texture object
-    GLuint Image_Format; // Format of loaded image
-    // Texture configuration
-    GLuint Wrap_S; // Wrapping mode on S axis
-    GLuint Wrap_T; // Wrapping mode on T axis
-    GLuint Filter_Min; // Filtering mode if texture pixels < screen pixels
-    GLuint Filter_Max; // Filtering mode if texture pixels > screen pixels
-    
-    // Generates texture from image data
-    void Generate(GLuint width, GLuint height, unsigned char* data);
-    // Binds the texture as the current active GL_TEXTURE_2D texture object
-    void Bind() const;
+  
+	GLuint ID;
+	GLenum type;
+	Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType);
 
-    void loadFromFile(const std::string& path);
+
+	void texUnit(Shader& shader, const char* uniform, GLuint unit);
+
+	void Bind();
+	void Unbind();
+	void Delete();
 };
 
 
