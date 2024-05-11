22190120c3afc6466cc8ec39a0512dacdbd63d22
Daniel Pascual López <uo269728@uniovi.es>
2024-05-11 15:23:15 +0200

    Limpieza clase textura

--- OpenGLEngine/Textures/Texture.hpp
+++ OpenGLEngine/Textures/Texture.hpp
@@ -5,22 +5,14 @@
 
 #include "Shader.hpp"
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 
 class Texture {
 public:
   
 	GLuint ID;
-	GLenum type;
-	Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType);
 
+	Texture();
 
-	void texUnit(Shader& shader, const char* uniform, GLuint unit);
-
-	void Bind();
-	void Unbind();
-	void Delete();
 };
 
 
