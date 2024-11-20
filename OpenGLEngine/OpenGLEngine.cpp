ca92654a11d83abaa3b5b706090245979f3aa5be
Daniel Pascual López <uo269728@uniovi.es>
2024-11-20 13:43:55 +0100

    Post Procesado

--- OpenGLEngine/OpenGLEngine.cpp
+++ OpenGLEngine/OpenGLEngine.cpp
@@ -9,6 +9,19 @@ namespace fs = std::filesystem;
 const unsigned int width = 800;
 const unsigned int height = 800;
 
+float rectangleVertices[] =
+{
+	// Coords    // texCoords
+	 1.0f, -1.0f,  1.0f, 0.0f,
+	-1.0f, -1.0f,  0.0f, 0.0f,
+	-1.0f,  1.0f,  0.0f, 1.0f,
+
+	 1.0f,  1.0f,  1.0f, 1.0f,
+	 1.0f, -1.0f,  1.0f, 0.0f,
+	-1.0f,  1.0f,  0.0f, 1.0f
+};
+
+
 
 int main()
 {
@@ -49,10 +62,13 @@ int main()
 	Shader shaderProgram("C:/Users/Daniel/Desktop/VFX/GraphicsEngine/OpenGLEngine/shaders/vert.glsl", "C:/Users/Daniel/Desktop/VFX/GraphicsEngine/OpenGLEngine/shaders/frag.glsl");
 	
 	
+	Shader frameBufferShaderProgram("C:/Users/Daniel/Desktop/VFX/GraphicsEngine/OpenGLEngine/shaders/frameBufferVert.glsl", "C:/Users/Daniel/Desktop/VFX/GraphicsEngine/OpenGLEngine/shaders/frameBufferFrag.glsl");
+	
+
+
+	
 	Shader outliningProgram("C:/Users/Daniel/Desktop/VFX/GraphicsEngine/OpenGLEngine/shaders/outliningVert.glsl", "C:/Users/Daniel/Desktop/VFX/GraphicsEngine/OpenGLEngine/shaders/outliningFrag.glsl");
 	Shader grassProgram("C:/Users/Daniel/Desktop/VFX/GraphicsEngine/OpenGLEngine/shaders/vert.glsl", "C:/Users/Daniel/Desktop/VFX/GraphicsEngine/OpenGLEngine/shaders/transfrag.glsl");
-
-
 
 	// Take care of all the light related things
 	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
@@ -63,6 +79,9 @@ int main()
 	shaderProgram.Activate();
 	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
 	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+	frameBufferShaderProgram.Activate();
+	glUniform1i(glGetUniformLocation(frameBufferShaderProgram.ID, "screenTexture"), 0);
+
 
 
 	grassProgram.Activate();
@@ -92,7 +111,7 @@ int main()
 	* Also note that this requires C++17, so go to Project Properties, C/C++, Language, and select C++17
 	*/
 	std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
-	std::string ground = "/resources/ground/scene.gltf";
+	std::string ground = "/resources/crow/scene.gltf";
 	std::string grass = "/resources/grass/scene.gltf";
 
 
@@ -101,11 +120,53 @@ int main()
 	Model grassM((parentDir + grass).c_str());
 
 
+	// Create a rectangle to render the framebuffer to
+	unsigned int rectVAO, rectVBO;
+	glGenVertexArrays(1, &rectVAO);
+	glGenBuffers(1, &rectVBO);
+	glBindVertexArray(rectVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, rectVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(rectangleVertices), &rectangleVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+	//Create a rectangle to render the framebuffer to
+
 	
 	double previousTime = 0.0f;
 	double currentTime = 0.0f;
 	double timeDiff;
 	unsigned int frames = 0;
+
+
+	//Create a framebuffer
+	unsigned int FBO;
+	glGenFramebuffers(1, &FBO);
+	glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
+	unsigned int frameBufferTexture;
+	glGenTextures(1, &frameBufferTexture);
+	glBindTexture(GL_TEXTURE_2D, frameBufferTexture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, frameBufferTexture, 0);
+
+	unsigned int RBO;
+	glGenRenderbuffers(1, &RBO);
+	glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO);
+
+	auto fboStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	if (fboStatus != GL_FRAMEBUFFER_COMPLETE)
+	{
+		std::cout << "Framebuffer is not complete!" << std::endl;
+	}
+
 
 
 	//Deactivate vSync
@@ -131,12 +192,16 @@ int main()
 		}
 
 
+		glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
+
 
 		// Specify the color of the background
 		glClearColor(0.85f, 0.85f, 0.90f, 1.0f);
 		// Clean the back buffer and depth buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT| GL_STENCIL_BUFFER_BIT);
 
+		glEnable(GL_DEPTH_TEST);
 		
 		// Updates and exports the camera matrix to the Vertex Shader
 		camera.updateMatrix(45.0f, 0.1f, 100.0f);
@@ -148,9 +213,8 @@ int main()
 
 		// Draw a model
 		model.Draw(shaderProgram, camera);
-		glDisable(GL_CULL_FACE);
-		grassM.Draw(grassProgram, camera);
-		glEnable(GL_CULL_FACE);
+		//glDisable(GL_CULL_FACE);
+
 
 		//glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
 		//glStencilMask(0x00);
@@ -164,6 +228,13 @@ int main()
 		//glStencilMask(0xFF);
 		//glStencilFunc(GL_ALWAYS, 0, 0xFF);
 		//glEnable(GL_DEPTH_TEST);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		frameBufferShaderProgram.Activate();
+		glBindVertexArray(rectVAO);
+		glDisable(GL_DEPTH_TEST);
+		glBindTexture(GL_TEXTURE_2D, frameBufferTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
